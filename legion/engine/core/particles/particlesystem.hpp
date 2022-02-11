4b440e7377aaef53bbe237d2f80b4a42e621c875
Rowan Ramsey <blazinram@gmail.com>
2022-02-11 23:46:26 +0100

    setting up the particle system

--- legion/engine/core/particles/particlesystem.hpp
+++ legion/engine/core/particles/particlesystem.hpp
@@ -1,9 +1,7 @@
 #pragma once
-#include <core/engine/system.hpp>
-#include <core/ecs/ecs.hpp>
-#include <core/time/time.hpp>
-#include <core/events/events.hpp>
-#include <core/engine/system.inl>
+#include<core/engine/system.hpp>
+#include<core/ecs/ecs.hpp>
+#include<core/particles/particleemitter.hpp>
 
 /**
  * @file particlesystem.hpp
@@ -12,25 +10,26 @@
 
 namespace legion::core
 {
-    struct particle_emitter;
-
-    template<typename policy>
-    struct particle_policy;
-
-    struct life_time
+    class ParticleSystem : public System<ParticleSystem>
     {
-        float age = 0.f;
-        float max = 1.f;
-    };
+        void Update(float deltaTime)
+        {
+            ecs::filter<particle_emitter<>> filter;
+            for (auto& entity : filter)
+            {
+                auto& emitter = entity.get_component<particle_emitter<>>().get();
+                emitter.spawnBuffer += deltaTime;
+                while (emitter.spawnBuffer >= emitter.spawnRate)
+                {
+                    emitter.spawnBuffer -= emitter.spawnRate;
+                }
 
-    class ParticleSystem final : public System<ParticleSystem>
-    {
-    public:
-        void setup();
-        void shutdown();
-        void update(time::span deltaTime);
-        void emitter_setup(L_MAYBEUNUSED events::component_creation<particle_emitter>& event);
-        void emit(particle_emitter& emitter, size_type count);
-        void maintenance(particle_emitter& emitter, float deltaTime);
+                for (auto& lifeTime : emitter.getBuffer<int>())
+                    lifeTime += deltaTime;
+
+                for (auto& [id, policy] : emitter.policies)
+                    policy.get();
+            }
+        }
     };
 }
