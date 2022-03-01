8dc50382b831f34399e4972130a437d9a14d425c
Raphael Alexander Priatama <37859544+Developer-The-Great@users.noreply.github.com>
2022-03-01 18:38:30 +0700

    Subfeature/add physx include and libs (#399)
    * add physx includes
    
    * add debug libs and test code
    
    * initialize physx scene and global variables
    
    * add missin include from rebase
    
    * revert example module changes
    
    * removed physx globals, added shutdown
    
    * review changes, forward declare physxStatics
    
    * PxPhysics to static
    
    * change to const char* to fix warning
    
    * made statics inline and added lockguards
    
    * same lock for setup and shutdown
    
    * change to spinlock

---
+++ legion/engine/physics/physx/systems/physx_physics_system.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <core/core.hpp>
+
+namespace physx
+{
+    class PxScene;
+};
+
+namespace legion::physics
+{
+    class PhysXPhysicsSystem final : public System<PhysXPhysicsSystem>
+    {
+    public:
+
+        virtual void setup();
+
+        virtual void shutdown();
+
+        void fixedUpdate(time::time_span<fast_time> deltaTime);
+
+    private:
+
+        struct PhysxStatics;
+
+        void lazyInitPhysXVariables();
+
+        void releasePhysXVariables();
+
+        void setupDefaultScene();
+
+        static constexpr float m_timeStep = 0.02f;
+
+        physx::PxScene* m_physxScene;
+    };
+};
