a792a79283e0a28c2457b65fd280d15ee401ff72
Raphael Priatama <raphaelpriatama@gmail.com>
2022-03-30 11:09:12 +0700

    time step debugger

---
+++ applications/sandbox/systems/physics_debugging_system.cpp
@@ -0,0 +1,35 @@
+#include "physics_debugging_system.hpp"
+#include <rendering/rendering.hpp>
+
+namespace legion::physics
+{
+    void PhysicsDebuggingSystem::setup()
+    {
+        app::InputSystem::createBinding<RequestFlipPhysicsContinuousState>(app::inputmap::method::M);
+        app::InputSystem::createBinding<RequestSinglePhysicsTick>(app::inputmap::method::N);
+        app::InputSystem::createBinding<RequestRaycastDebug>(app::inputmap::method::MOUSE_LEFT);
+
+        bindToEvent<RequestFlipPhysicsContinuousState,
+            &PhysicsDebuggingSystem::raiseFlipContinuousUpdateEvent>();
+
+        bindToEvent<RequestSinglePhysicsTick,
+            &PhysicsDebuggingSystem::raiseSinglePhysicsTickEvent>();
+    }
+
+    void PhysicsDebuggingSystem::raiseFlipContinuousUpdateEvent(RequestFlipPhysicsContinuousState& action)
+    {
+        if (action.value)
+        {
+            m_isPhysicsContinuousUpdate = !m_isPhysicsContinuousUpdate;
+            raiseEvent<request_flip_physics_continuous>(request_flip_physics_continuous{ m_isPhysicsContinuousUpdate });
+        }
+    }
+
+    void PhysicsDebuggingSystem::raiseSinglePhysicsTickEvent(RequestSinglePhysicsTick& action)
+    {
+        if (!m_isPhysicsContinuousUpdate && action.value)
+        {
+            raiseEvent<request_single_physics_tick>();
+        }
+    }
+}
